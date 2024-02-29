6e75d49067ec03bab63d3b8b7be8eea34da46e32
gngtwhh <2784126850@qq.com>
2024-02-29 19:35:07 +0800

    start tea

--- test/Encoding/hex/main.c
+++ test/Encoding/hex/main.c
@@ -5,12 +5,12 @@
 #include <string.h>
 
 int main() {
-    byte bytes[100];
+    byte bytes[] = "hello world!";
     size_t out_len;
-    char hex[100] = "68656c6c6f20776f726c6421";
-    // bytes_to_hex(bytes, strlen(bytes), hex, &out_len);
-    // puts(hex);
-    hex_to_bytes(hex,strlen(hex),bytes,&out_len);
+    char hex[100];
+    bytes_to_hex(bytes, strlen(bytes), hex, &out_len);
+    puts(hex);
+    hex_to_bytes(hex, strlen(hex), bytes, &out_len);
     puts(bytes);
     return 0;
 }
