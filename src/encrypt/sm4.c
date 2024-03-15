448a2dac19e0b4a09705639a22539ead0395cfa1
gngtwhh <2784126850@qq.com>
2024-03-15 16:27:41 +0800

    feat: The file header of the SM4 encryption algorithm is planned

---
+++ src/encrypt/sm4.c
@@ -0,0 +1,9 @@
+/**
+ * @file: SM4.c
+ * @description: This is a source file that contains the implementation of RC4 encryption algorithm.
+ * @author: WAHAHA
+ * @Date: 2024/3/15 15:49
+ * @LastEditTime: 2024/3/15 15:49
+ * @FilePath: Serendipity/src/encrypt/SM4.c
+ * @category: 
+ */
---
+++ test/Encrypt/SM4/main.c
@@ -0,0 +1,8 @@
+#include <encrypt/sm4.h>
+#include <string.h>
+
+int main() {
+
+
+    return 0;
+}
