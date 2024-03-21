f42108599f84f30d23107c7c0b22ffebe094761c
gngtwhh <2784126850@qq.com>
2024-03-21 15:16:14 +0800

    feat: Create pkcspad.c and pkcspad.h

---
+++ include/encrypt/pkcspad.h
@@ -0,0 +1,15 @@
+/**
+ * @file: pkcspad.h
+ * @description: This is a header file that contains the declaration of xxx
+ * @author: WAHAHA
+ * @Date: 2024/3/19 2:29
+ * @LastEditTime: 2024/3/19 2:29
+ * @FilePath: Serendipity/include/encrypt/pkcspad.h
+ * @category: 
+ */
+
+#ifndef SERENDIPITY_PKCSPAD_H
+#define SERENDIPITY_PKCSPAD_H
+
+
+#endif //SERENDIPITY_PKCSPAD_H
---
+++ src/encrypt/pkcspad.c
@@ -0,0 +1,9 @@
+/**
+ * @file: pkcspad.c
+ * @description: This is a source file that contains the implementation of xxx
+ * @author: WAHAHA
+ * @Date: 2024/3/19 2:29
+ * @LastEditTime: 2024/3/19 2:29
+ * @FilePath: Serendipity/src/encrypt/pkcspad.c
+ * @category: 
+ */
