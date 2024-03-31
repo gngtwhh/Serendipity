11a115b665e8d9bcf2a8d4600dfd18276b522d4b
gngtwhh <2784126850@qq.com>
2024-03-31 18:29:08 +0800

    feat: Add documentation for the "hex" module and modify the test sample.

--- test/Encoding/hex/main.c
+++ test/Encoding/hex/main.c
@@ -1,16 +1,21 @@
 #include "encode/hex.h"
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 int main() {
     byte bytes[] = "hello world!";
     size_t out_len;
-    char hex[100];
-    bytes_to_hex(bytes, strlen(bytes), hex, &out_len);
-    puts(hex);
-    hex_to_bytes(hex, strlen(hex), bytes, &out_len);
-    puts(bytes);
+    char hex_test1[100];
+    bytes_to_hex(bytes, strlen((char *) bytes), hex_test1, &out_len);
+    puts(hex_test1);
+    hex_to_bytes(hex_test1, strlen(hex_test1), bytes, &out_len);
+    bytes[out_len] = '\0';
+    puts((char *) bytes);
+
+    char hex_test2[] = "68656c6c6f20776fk26c6421";
+    hex_to_bytes(hex_test2, strlen(hex_test2), bytes, &out_len);
+    bytes[out_len] = '\0';
+    puts((char *) bytes);
     return 0;
 }
