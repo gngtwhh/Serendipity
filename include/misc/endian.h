563184d97bbcded8cb524a7f47bb492d003dbdd9
gngtwhh <2784126850@qq.com>
2024-06-01 20:08:42 +0800

    feat: Add endian conversion of 8bytes data(uint64)
    Now "endian.h" supports conversion between bytes and uint64, which is used in the "SHA2" module.

--- include/misc/endian.h
+++ include/misc/endian.h
@@ -15,6 +15,7 @@
  * Convert 4 bytes of data to uint32_t in big-endian or little-endian
  */
 /* big-endian */
+// u: uint32_t, b: unsigned char *
 #define BE_BYTES_TO_UINT32(u, b)                     \
     ((u) = (uint32_t)((b)[0]) << 24                  \
          | (uint32_t)((b)[1]) << 16                  \
@@ -22,7 +23,7 @@
          | (uint32_t)((b)[3])                        \
     )
 
-
+// b: unsigned char *, u: uint32_t
 #define BE_UINT32_TO_BYTES(b, u)                     \
     do {                                             \
         (b)[0] = (unsigned char)((u) >> 24);         \
@@ -31,7 +32,33 @@
         (b)[3] = (unsigned char)( u       );         \
     } while(0)
 
+// b: unsigned char *, u: uint64_t
+#define BE_BYTES_TO_UINT64(b, u)                     \
+    ((u) = (uint64_t)((b)[0]) << 56                  \
+         | (uint64_t)((b)[1]) << 48                  \
+         | (uint64_t)((b)[2]) << 40                  \
+         | (uint64_t)((b)[3]) << 32                  \
+         | (uint64_t)((b)[4]) << 24                  \
+         | (uint64_t)((b)[5]) << 16                  \
+         | (uint64_t)((b)[6]) <<  8                  \
+         | (uint64_t)((b)[7])                        \
+    )
+
+// u: uint64_t, b: unsigned char *
+#define BE_UINT64_TO_BYTES(b, u)                     \
+    do {                                             \
+        (b)[0] = (unsigned char)((u) >> 56);         \
+        (b)[1] = (unsigned char)((u) >> 48);         \
+        (b)[2] = (unsigned char)((u) >> 40);         \
+        (b)[3] = (unsigned char)((u) >> 32);         \
+        (b)[4] = (unsigned char)((u) >> 24);         \
+        (b)[5] = (unsigned char)((u) >> 16);         \
+        (b)[6] = (unsigned char)((u) >>  8);         \
+        (b)[7] = (unsigned char)( u       );         \
+    } while(0)
+
 /* little-endian */
+// u: uint32_t, b: unsigned char *
 #define LE_BYTES_TO_UINT32(u, b)                     \
     ((u) = (uint32_t)((b)[3]) << 24                  \
          | (uint32_t)((b)[2]) << 16                  \
@@ -39,6 +66,7 @@
          | (uint32_t)((b)[0])                        \
     )
 
+// b: unsigned char *, u: uint32_t
 #define LE_UINT32_TO_BYTES(b, u)                     \
     do {                                             \
         (b)[3] = (unsigned char)((u) >> 24);         \
@@ -47,5 +75,29 @@
         (b)[0] = (unsigned char)( u       );         \
     } while(0)
 
+// b: unsigned char *, u: uint64_t
+#define LE_BYTES_TO_UINT64(b, u)                     \
+    ((u) = (uint64_t)((b)[7]) << 56                  \
+         | (uint64_t)((b)[6]) << 48                  \
+         | (uint64_t)((b)[5]) << 40                  \
+         | (uint64_t)((b)[4]) << 32                  \
+         | (uint64_t)((b)[3]) << 24                  \
+         | (uint64_t)((b)[2]) << 16                  \
+         | (uint64_t)((b)[1]) <<  8                  \
+         | (uint64_t)((b)[0])                        \
+    )
+
+// u: uint64_t, b: unsigned char *
+#define LE_UINT64_TO_BYTES(b, u)                     \
+    do {                                             \
+        (b)[7] = (unsigned char)((u) >> 56);         \
+        (b)[6] = (unsigned char)((u) >> 48);         \
+        (b)[5] = (unsigned char)((u) >> 40);         \
+        (b)[4] = (unsigned char)((u) >> 32);         \
+        (b)[3] = (unsigned char)((u) >> 24);         \
+        (b)[2] = (unsigned char)((u) >> 16);         \
+        (b)[1] = (unsigned char)((u) >>  8);         \
+        (b)[0] = (unsigned char)( u       );         \
+    } while(0)
 
 #endif //SERENDIPITY_ENDIAN_H
