fe4cf870a576687153638b032911a3ceb2a111a7
gngtwhh <2784126850@qq.com>
2025-03-27 19:35:52 +0800

    test: Update hex test code

--- test/Encoding/hex/main.c
+++ test/Encoding/hex/main.c
@@ -4,18 +4,20 @@
 #include <string.h>
 
 int main() {
-    byte bytes[] = "hello world!";
-    size_t out_len;
-    char hex_test1[100];
-    bytes_to_hex(bytes, strlen((char *) bytes), hex_test1, &out_len);
-    puts(hex_test1);
-    hex_to_bytes(hex_test1, strlen(hex_test1), bytes, &out_len);
-    bytes[out_len] = '\0';
-    puts((char *) bytes);
+    char text[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}[]|\\:;\"'<>,.?/~`";
+    size_t text_len, hex_len;
+    byte hex[256] = {0};
 
-    char hex_test2[] = "68656c6c6f20776fk26c6421";
-    hex_to_bytes(hex_test2, strlen(hex_test2), bytes, &out_len);
-    bytes[out_len] = '\0';
-    puts((char *) bytes);
+    /* bytes to hex */
+    bytes_to_hex(text, strlen(text), hex, &hex_len);
+    for (int i = 0; i < hex_len; ++i)
+        printf("%2X", hex[i]);
+    putchar('\n');
+    /* hex to bytes */
+    memset(text, 0, strlen(text));
+    hex_to_bytes(hex, hex_len, text, &text_len);
+    for (int i = 0; i < text_len; ++i)
+        putchar(text[i]);
+
     return 0;
 }
